674a487afd3fcccbac89f44c248eb72af34a64ed
Steve Duitsman <steve.duitsman+github@gmail.com>
2012-09-29 14:55:56 -0500

    Weather Page Formatting

--- KnockoutJS_Show_and_Tell/KnockoutJS_Show_and_Tell/Scripts/app/WeatherViewModel.js
+++ KnockoutJS_Show_and_Tell/KnockoutJS_Show_and_Tell/Scripts/app/WeatherViewModel.js
@@ -1,12 +1,17 @@
 ﻿function WeatherViewModel(config) {
     var self = this;
+
     self.Config = config;
+
     self.Cities = ko.observableArray([]);
+    self.SelectedCity = ko.observable();
+    self.FilteredWeather = ko.observableArray([]);
     self.Weather = ko.observableArray([]);
 
     self.FetchCities = function (callback) {
         return $.getJSON(self.Config.Urls.GetCities, function (data) {
             ko.mapping.fromJS(data, {}, self.Cities);
+            self.SelectedCity(78201);
             if (callback !== undefined && typeof (callback) == "function") {
                 callback();
             }
@@ -22,33 +27,32 @@
         });
     };
 
-    self.FilterWeather = function (zip) {
+    self.IsCitySelected = function(zip) {
+        return self.SelectedCity() === zip;
+    };
+
+    self.SelectCity = function (zip) {
         if (zip === null || zip == undefined) {
-        } else {
-        }
+            var filteredData = ko.utils.arrayFilter(self.Cities, function (item) {
+                var match = false;
+                var locationData = item.display_location() || undefined;
+                if (locationData !== undefined) {
+                    var zipData = locationData.zip() || undefined;
+                    if (zipData !== undefined) {
+                        match = ko.utils.stringStartsWith(zipData, zip);
+                    }
+                }
+                return match;
+            });
+            self.SelectedCity(zip);
+            self.FilteredWeather(filteredData);
+        } 
     };
-
-    //self.Fetch = function () {
-    //    var deferred = $.Deferred();
-
-    //    $.when(self.FetchCities(), self.FetchWeather())
-    //     .done(deferred.resolve);
-
-    //    //var cityDeferred = $.Deferred();
-    //    //self.FetchCities(cityDeferred.resolve);
-    //    //var weatherDeferred = $.Deferred();
-    //    //self.FetchWeather(weatherDeferred.resolve);
-    //    //if (cityDeferred.isResolved() && weatherDeferred.isResolved()) {
-    //    //    deferred.resolve();
-    //    //}
-    //    return deferred.promise();
+    //return {
+    //    //Fetch: self.Fetch,
+    //    FetchCities: self.FetchCities,
+    //    Cities: ko.observable(self.Cities),
+    //    FetchWeather: self.FetchWeather,
+    //    Weather: ko.observable(self.Weather)
     //};
-
-    return {
-        //Fetch: self.Fetch,
-        FetchCities: self.FetchCities,
-        Cities: ko.observable(self.Cities),
-        FetchWeather: self.FetchWeather,
-        Weather: ko.observable(self.Weather)
-    };
 };
